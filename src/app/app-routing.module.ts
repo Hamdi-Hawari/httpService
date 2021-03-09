71c61dc2ad732a4ae5bf115168a8e48540ff203e
hamdi <hamdi.hawari@gmx.de>
2021-03-09 16:21:10 +0100

    neue Komponent erstellen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AboutComponent} from './about/about.component';
+import {LogComponent} from './log/log.component';
+import {ContactComponent} from './contact/contact.component';
+import {HomeComponent} from './home/home.component';
+import {HttpServiceComponent} from './http-service/http-service.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'about',
+    component: AboutComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'log',
+    component: LogComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'contact',
+    component: ContactComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  },
+  {
+    path: 'service',
+    component: HttpServiceComponent,
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
+import { LogComponent } from './log/log.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { HttpServiceComponent } from './http-service/http-service.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ToolbarComponent,
     FooterComponent,
-    AboutComponent
+    AboutComponent,
+    LogComponent,
+    ContactComponent,
+    HomeComponent,
+    HttpServiceComponent,
+    EmployeeListComponent
   ],
   imports: [
     BrowserModule,
